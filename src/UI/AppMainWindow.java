25723649e15f04b12efd88509f2c7f1e892d7db8
DraLastat <dralastat@163.com>
2017-06-30 19:39:23 +0800

    add window listener

--- src/UI/AppMainWindow.java
+++ src/UI/AppMainWindow.java
@@ -107,5 +107,52 @@ public class AppMainWindow {
         frame.add(mainPanel);
 
 
+        frame.addWindowListener(new WindowListener() {
+
+            @Override
+            public void windowOpened(WindowEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+
+            	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                logger.info("==================BricksEnd==================");
+                 //Dan to adding here
+
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+        });
+        
+    
     }
 }
