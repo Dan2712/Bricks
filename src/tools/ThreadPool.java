542f3af85f9fcaf44f29fb679215d25c7c8ace24
dan <dan.ge@dji.com>
2017-07-06 22:11:24 +0800

    start thread refine

---
+++ src/tools/ThreadPool.java
@@ -0,0 +1,9 @@
+package tools;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPool {
+	ExecutorService cachedThreadPool = Executors.newCachedThreadPool();
+	
+}
