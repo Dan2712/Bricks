61df4e94ea1e91d494391b31066bb1fdd242db52
dan <dan.ge@dji.com>
2018-06-08 16:03:18 +0800

    change to precise xpath

--- src/com/dji/bricks/node_selection/RealTimeScreenUI.java
+++ src/com/dji/bricks/node_selection/RealTimeScreenUI.java
@@ -392,7 +392,7 @@ public class RealTimeScreenUI extends JPanel implements GlobalObserver, MouseLis
 	            	mModel.setSelectedNode(node);
 	            	UiNode node_sel = (UiNode) node;
 	            	node_info.clear();
-	            	node_info.put("xpath", "/" + node_sel.getXpath());
+	            	node_info.put("xpath", node_sel.getAttribute("fullIndexXpath").substring(1));
 	            	node_info.put("clickable", node_sel.getAttribute("clickable").equals("true") ? "1" : "0");
 	            	node_info.put("scrollable", node_sel.getAttribute("scrollable").equals("true") ? "1" : "0");
 	            	node_info.put("checkable", node_sel.getAttribute("checkable").equals("true") ? "1" : "0");
